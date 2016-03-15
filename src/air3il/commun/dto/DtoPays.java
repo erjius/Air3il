4aee3837dcbf341690aed0babf878b29ea93aa1e
spielmann <antoinespielmann@yahoo.fr>
2016-03-15 14:58:20 +0100

    ajout des dto pays et ville

---
+++ src/air3il/commun/dto/DtoPays.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package air3il.commun.dto;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author antoine
+ */
+public class DtoPays implements Serializable {
+
+    private int id;
+
+    private String nom;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public DtoPays() {
+        super();
+    }
+
+    public DtoPays(int id, String nom) {
+        super();
+        this.id = id;
+        this.nom = nom;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final DtoPays other = (DtoPays) obj;
+        return this.id == other.id;
+    }
+    
+    
+}
---
+++ src/air3il/commun/dto/DtoVille.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package air3il.commun.dto;
+
+/**
+ *
+ * @author antoine
+ */
+public class DtoVille {
+
+    private int id;
+
+    private String nom;
+
+    private DtoPays pays;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public DtoPays getPays() {
+        return pays;
+    }
+
+    public void setPays(DtoPays pays) {
+        this.pays = pays;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final DtoVille other = (DtoVille) obj;
+        return this.id == other.id;
+    }
+}

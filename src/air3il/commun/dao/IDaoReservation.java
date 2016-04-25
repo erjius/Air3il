54f35f9b2e5da645e5bf6c47ae35a7c4afaa9253
ARSENE <ARSENE@DESKTOP-3AKSG80>
2016-04-25 14:01:13 +0200

    modifier char en string pour num_pass

--- src/air3il/commun/dao/IDaoReservation.java
+++ src/air3il/commun/dao/IDaoReservation.java
@@ -5,13 +5,12 @@ import java.util.List;
 import air3il.commun.dto.DtoReservation;
 import air3il.commun.dto.DtoVol;
 import air3il.commun.dto.DtoPlace;
-import air3il.commun.dto.DtoClient;
 
 public interface IDaoReservation {
 
-    public DtoReservation inserer(DtoReservation reservation);
+    public void inserer(DtoReservation reservation);
 
-    public DtoReservation modifier(DtoReservation reservation);
+    public void modifier(DtoReservation reservation);
 
     public void supprimer(DtoVol vol, DtoPlace place);
 

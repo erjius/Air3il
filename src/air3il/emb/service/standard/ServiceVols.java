e18867797946a0db8644de616c73987a5f0bada8
lilip <lilip@172.18.102.115>
2016-05-04 14:18:04 +0200

    remplir les mothodes pour Leonel

--- src/air3il/emb/service/standard/ServiceVols.java
+++ src/air3il/emb/service/standard/ServiceVols.java
@@ -1,30 +1,40 @@
 package air3il.emb.service.standard;
 
-import air3il.commun.dao.IDaoPays;
-import air3il.commun.dao.IDaoVille;
-import air3il.commun.dao.IDaoVol;
-import air3il.commun.dao.IManagerDao;
 import java.util.List;
 import air3il.commun.dto.DtoVille;
 import air3il.commun.dto.DtoVol;
 import air3il.commun.exception.ExceptionAppli;
 import air3il.commun.service.IServiceVols;
+import air3il.emb.dao.jdbc.ManagerDao;
+import air3il.emb.dao.test.DaoVol;
+import air3il.gui.javafx.systeme.ModelSysteme;
+import air3il.gui.javafx.vols.ModelVols;
 import java.util.Date;
-import java.util.Map;
-import java.util.TreeSet;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 
-public class ServiceVols implements IServiceVols {
+public class ServiceVols implements IServiceVols{
    
     // Logger
     private static final Logger logger = Logger.getLogger(IServiceVols.class.getName());
 
+
     // Champs 
-    private IDaoPays iDaoPays;
-    private IDaoVille iDeoville;
-    private IDaoVol idaoVol;
-    private IManagerDao managerDao;
+    private ModelSysteme model;
+    private ManagerDao managerDao;
+    private DaoVol daoVol;
+    // Injecteurs
+    
+    public void setModel(ModelSysteme model) {
+        this.model = model;
+    }
+
+    
+    public void setManagerDao(ManagerDao managerDao) {
+        this.managerDao = managerDao;
+        this.daoVol = managerDao.getDao(DaoVol.class);
+    }
     
 
     @Override
@@ -44,12 +54,22 @@ public class ServiceVols implements IServiceVols {
 
     @Override
     public List<DtoVol> lstToutVol() throws ExceptionAppli {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            return daoVol.listerTout();
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, e.getMessage(), e);
+            throw new ExceptionAppli(e);
+        }
     }
 
     @Override
     public List<DtoVol> listervols(DtoVille depart, DtoVille arrive, Date date) throws ExceptionAppli {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+           return daoVol.recherher(arrive, depart, date);
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, e.getMessage(), e);
+            throw new ExceptionAppli(e);
+        }
     }
 
     @Override
